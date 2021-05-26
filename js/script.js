458516d3bb3271606e6820d34fa754b1c98da2a0
Fisny <bardi.fisniku@gmail.com>
2021-05-26 17:16:47 +0200

    java

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//esplora-ps4
+
+document.getElementById('myID').innerHTML = 'Entra anche tu nella Comunity!';
+
+alert("Benvenuto, prima di proseguire con la visione di questo sito ti preghiamo di accettare la normativa sulla tutela dei dati per garantire la sicurezza dei nostri utenti.");
